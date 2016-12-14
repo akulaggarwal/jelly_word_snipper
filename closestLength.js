69652880c18a7a10f83d943ca2778bf639531241
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-12-14 13:47:43 -0800

    Basic requirements met in closestLength.js

--- closestLength.js
+++ closestLength.js
@@ -1,5 +1,4 @@
 const originalStr = 'What I highly recommended is creating hobbies together and exploring new things together. When life becomes dull and has become a stalemate routine then this affects communication. Take a trip to the theaters for example. After the movie people usually proceed to have a discussion about it afterwards. Couples need to have new experiences and constantly push themselves out of that same day to day routine, or else that routine will slowly kill the relationship one step at a time.'
-
 const sampleSnip = 'What I highly recommended is creating hobbies together and exploring new things together.'
 
 console.log(sampleSnip.length);
@@ -8,22 +7,46 @@ console.log(sampleSnip.length);
 const idealLen = 100;
 const maxLen = 200;
 const minLen = 80;
-const punctuations = ['.', ';', ',', ':', ')', '-'];
+const punctuations = ['.', ';', ',', ')', '-', , ':'];
 
+// function closestPunctuation(punctArr) {
+//   punctArr.reduce( (min, cur) => {
+//     const diff = cur - idealLen;
+//     if (diff < min) {
+//       return diff
+//     } else {
+//       return min
+//     }
+//   }, 101)
+// }
 
-function closestPunctuation(punctArr) {
-  punctArr.reduce( (min, cur) => {
-    const diff = cur - idealLen;
-    if (diff < min) {
-      return diff
-    } else {
-      return min
+// function findPunctuation(punctuation, inputStr) {
+//   //REGEX: use regex or something to return array with all numerical locations with input punctuation
+// }
+
+function findPunctuation(punctuation, inputStr) {
+  const punctuationLocations = [];
+  for (let i = 0; i < 100; i++) {
+    const offset = idealLen - 1;
+    const strForwards = inputStr[offset + i];
+    const strBackwards = inputStr[offset - i];
+    if (strForwards === punctuation) {
+      punctuationLocations.push(offset + i);
     }
-  }, 101)
-}
+    if (strBackwards === punctuation && i <= idealLen - minLen && i) {
+      punctuationLocations.push(offset - i);
+    }
 
-function findPunctuation(punctuation) {
-  //REGEX: use regex or something to return array with all numerical locations with input punctuation
+    if (punctuationLocations.length) {
+      break;
+    }
+  }
+
+  if (punctuationLocations.length) {
+    return punctuationLocations[0];
+  }
+
+  return null;
 }
 
 function makeSnip(inputStr) {
@@ -33,19 +56,24 @@ function makeSnip(inputStr) {
     return inputStr
   }
 
-  let possibleSnipLocations = null;
+  let snipLocation = null;
   for (let val of punctuations) {
-    possibleSnipLocations = findPunctuation(val);
+    snipLocation = findPunctuation(val, inputStr);
 
-    if (possibleSnipLocations) {
+    if (snipLocation) {
       break;
     }
   }
 
-  const snipLocation = closestPunctuation(periods);
-  const snippet = inputStr.slice(0, snipLocation + 1);
-  const remaining = inputStr.slice(snipLocation + 1);
+  //In case there's somehow no punctuation...
+  // if (/*no punc within first 200 chars*/) {
+    //return closest word to idealLen
+  // }
 
+  // const snipLocation = closestPunctuation(possibleSnipLocations);
+  const snippet = inputStr.slice(0, snipLocation + 1);
+  const remaining = inputStr.slice(snipLocation + 2);
+  console.log({snippet, remaining});
   return {snippet, remaining}
 }
 
