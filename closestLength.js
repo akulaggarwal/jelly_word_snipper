8c865f581815f0fb890638b7fd28fd1ac8786b60
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-12-14 15:05:29 -0800

    Clean up basic working code

--- closestLength.js
+++ closestLength.js
@@ -1,8 +1,6 @@
 const originalStr = 'What I highly recommended is creating hobbies together and exploring new things together. When life becomes dull and has become a stalemate routine then this affects communication. Take a trip to the theaters for example. After the movie people usually proceed to have a discussion about it afterwards. Couples need to have new experiences and constantly push themselves out of that same day to day routine, or else that routine will slowly kill the relationship one step at a time.'
 const sampleSnip = 'What I highly recommended is creating hobbies together and exploring new things together.'
 
-console.log(sampleSnip.length);
-
 //Changeable paramaters (the knobs):
 const idealLen = 100;
 const maxLen = 200;
@@ -11,10 +9,12 @@ const punctuations = ['.', '!', ';', '?', ',', ')', '-', , ':'];
 
 function findPunctuation(punctuation, inputStr) {
   const punctuationLocations = [];
-  for (let i = 0; i < 100; i++) {
+
+  for (let i = 0; i < (maxLen - idealLen); i++) {
     const offset = idealLen - 1;
     const strForwards = inputStr[offset + i];
     const strBackwards = inputStr[offset - i];
+
     if (strForwards === punctuation) {
       punctuationLocations.push(offset + i);
     }
@@ -35,10 +35,9 @@ function findPunctuation(punctuation, inputStr) {
 }
 
 function makeSnip(inputStr) {
-
-  //handle case for smaller than minimum input
+  //Edge-case: handle case for smaller than minimum input
   if (inputStr.length < minLen){
-    return inputStr
+    return {snippet: inputStr}
   }
 
   let snipLocation = null;
@@ -50,15 +49,14 @@ function makeSnip(inputStr) {
     }
   }
 
-  //In case there's somehow no punctuation at all! ...
-  if (!snipLocation) {
-    findPunctuation(' ', inputStr);
-  }
+  //Edge-case: In case there's somehow no punctuation at all! ...
+  snipLocation = snipLocation ? snipLocation: findPunctuation(' ', inputStr);
 
   const snippet = inputStr.slice(0, snipLocation + 1);
   const remaining = inputStr.slice(snipLocation + 2);
-  console.log({snippet, remaining});
+
   return {snippet, remaining}
 }
 
-makeSnip(originalStr)
+const snipped = makeSnip(originalStr);
+console.log(snipped);
